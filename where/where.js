aac0457c926ec23f6986b46a7a8668840a1b65e1
Beckylee <Beckylee.Dell@tufts.edu>
2013-03-03 19:03:00 -0500

    i have a good feeling about this time

--- where/where.js
+++ where/where.js
@@ -365,7 +365,7 @@ function init(lat, lng)
         var dLat  = deg2rad(mlat - lat);
         var dLong = deg2rad(mlng - lng);
         var a =     Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.cos(deg2rad(lat)) * Math.cos(deg2rad(mlat)) * 
     Math.sin(dLon/2) * Math.sin(dLon/2);
         var c =  2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
         var d = R * c;
